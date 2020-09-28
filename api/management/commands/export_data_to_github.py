a5ee22dbc4dbea12b3bad8d0f6278b24d3f5dee6
Raphael Odini <raphael.odini@protonmail.com>
2020-09-28 20:22:52 +0200

    temp

--- api/management/commands/export_data_to_github.py
+++ api/management/commands/export_data_to_github.py
@@ -22,8 +22,8 @@ class Command(BaseCommand):
             "%Y-%m-%d %H:%M:%S"
         )  # YYYY-MM-DD HH:MM:SS
         current_date_string = current_datetime.strftime("%Y-%m-%d")  # YYYY-MM-DD
-        data_update_branch_name = f"data-update-{current_date_string}"
-        data_update_pull_request_name = f"Data: update ({current_date_string})"
+        data_update_branch_name = f"data-update-{current_date_string}-test"
+        # data_update_pull_request_name = f"Data: update ({current_date_string})"
 
         # create branch
         utilities_github.create_branch(data_update_branch_name)
@@ -31,47 +31,48 @@ class Command(BaseCommand):
         # update & commit files
         # data/questions.yaml, data/quizzes.yaml, data/tags.yaml
         # frontend/src/constants.js
-        questions_yaml = utilities.serialize_model_to_yaml(model_label="question")
-        utilities_github.create_file(
-            file_path="data/questions.yaml",
-            commit_message="update questions",
-            file_content=questions_yaml,
-            branch_name=data_update_branch_name,
-        )
-        quizzes_yaml = utilities.serialize_model_to_yaml(model_label="quiz")
-        utilities_github.create_file(
-            file_path="data/quizzes.yaml",
-            commit_message="update quizzes",
-            file_content=quizzes_yaml,
-            branch_name=data_update_branch_name,
-        )
-        tags_yaml = utilities.serialize_model_to_yaml(model_label="tag")
-        utilities_github.create_file(
-            file_path="data/tags.yaml",
-            commit_message="update tags",
-            file_content=tags_yaml,
-            branch_name=data_update_branch_name,
-        )
-        old_frontend_constants_file_content = utilities_github.get_file(
+        # questions_yaml = utilities.serialize_model_to_yaml(model_label="question")
+        # utilities_github.create_file(
+        #     file_path="data/questions.yaml",
+        #     commit_message="update questions",
+        #     file_content=questions_yaml,
+        #     branch_name=data_update_branch_name,
+        # )
+        # quizzes_yaml = utilities.serialize_model_to_yaml(model_label="quiz")
+        # utilities_github.create_file(
+        #     file_path="data/quizzes.yaml",
+        #     commit_message="update quizzes",
+        #     file_content=quizzes_yaml,
+        #     branch_name=data_update_branch_name,
+        # )
+        # tags_yaml = utilities.serialize_model_to_yaml(model_label="tag")
+        # utilities_github.create_file(
+        #     file_path="data/tags.yaml",
+        #     commit_message="update tags",
+        #     file_content=tags_yaml,
+        #     branch_name=data_update_branch_name,
+        # )
+        old_frontend_constants_file = utilities_github.get_file(
             file_path="frontend/src/constants.js", branch_name=data_update_branch_name
         )
         new_frontend_constants_file_content = utilities.update_frontend_last_updated_datetime(
-            old_frontend_constants_file_content, current_datetime_string
+            old_frontend_constants_file.decoded_content.decode(),
+            current_datetime_string,
         )
         utilities_github.update_file(
             file_path="frontend/src/constants.js",
             commit_message="update frontend constants (last_updated datetime)",
             file_content=new_frontend_constants_file_content,
-            branch_name=data_update_branch_name,
+            branch_name="master",
         )
 
-        # create pull request
-        pull_request = utilities_github.create_pull_request(
-            pull_request_title=data_update_pull_request_name,
-            pull_request_message="Mise à jour de la donnée : <ul><li>data/questions.yaml</li><li>data/quizzes.yaml</li><li>data/tags.yaml</li></ul>",  # noqa
-            branch_name=data_update_branch_name,
-        )
-        print(pull_request.html_url)
+        # # create pull request
+        # pull_request = utilities_github.create_pull_request(
+        #     pull_request_title=data_update_pull_request_name,
+        #     pull_request_message="Mise à jour de la donnée : <ul><li>data/questions.yaml</li><li>data/quizzes.yaml</li><li>data/tags.yaml</li></ul>",  # noqa
+        #     branch_name=data_update_branch_name,
+        # )
+        # print(pull_request.html_url)
 
-        # return
-        self.stdout.write(pull_request.html_url)
+        # # return
+        # self.stdout.write(pull_request.html_url)
